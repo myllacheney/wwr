6e42cadaeff0518a4b91ea201d70f2d82957b946
Myllayd Cheney <che21009@byui.edu>
2023-09-20 20:00:22 -0600

    adding other folders

---
+++ Python/favoritecolor.py
@@ -0,0 +1,4 @@
+favoritecolor = input('What is your favorite color?')
+print(f'Your favorite color is {favoritecolor}')
+answer = input('Why is this your favorite color?')
+print ('I like this color too!')
---
+++ Python/madlib.py
@@ -0,0 +1,23 @@
+'''
+The other day, I was really in trouble. It all started when I saw a very
+[adjective] [animal] [verb] down the hallway. "[exclamation]!" I yelled. But all
+I could think to do was to [verb] over and over. Miraculously,
+that caused it to stop, but not before it tried to [verb]
+right in front of my family.
+'''
+print('Please enter the following:')
+print()
+adjective = input('adjective: ')
+animal = input('animal: ')
+verb1 = input('verb: ')
+exclamation = input('exclamation: ')
+verb2 = input('verb: ')
+verb3 = input('verb: ')
+print()
+print('Your store is: ')
+print()
+print('The other day, I was really in trouble. It all started when I saw a very')
+print(f'{adjective.lower()} {animal.lower()} {verb1.lower()} down the hallway. "{exclamation.upper()}!" I yelled. But all')
+print(f'I could think to do was to {verb2.lower()} over and over. Miraculously,')
+print(f'that caused it to stop, but not before it tried to {verb3.lower()}')
+print('right in front of my family.')
