987d5cde68d59c50dfb0dcba6f98e88658a1e23f
Myllayd Cheney <che21009@byui.edu>
2023-09-20 20:08:01 -0600

    removing files

--- Python/boxes.py
+++
@@ -1,7 +0,0 @@
-import math
-number_items = int(input('Enter the number of items: '))
-items_per_box = int(input('Enter the number of items per box: '))
-number_boxes = math.ceil(number_items/items_per_box)
-print()
-print(f'For {number_items} items, packing {items_per_box} items in each box, you will need {number_boxes} boxes. ')
-
--- Python/favoritecolor.py
+++
@@ -1,4 +0,0 @@
-favoritecolor = input('What is your favorite color?')
-print(f'Your favorite color is {favoritecolor}')
-answer = input('Why is this your favorite color?')
-print ('I like this color too!')
